ccd6960ca49798c013dc321563894903ffa39c90
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-06 17:00:24 -0500

    study questions

--- client/src/components/PlayerCard.test.js
+++ client/src/components/PlayerCard.test.js
@@ -4,7 +4,7 @@ import PlayerCard from './PlayerCard';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-test('Player Card fucking does something ANYTHING PLEASE', () => {
+test('Player Card does something ANYTHING PLEASE', () => {
     let fakePlayer = {
         name: 'Janey',
         country: 'United States',
@@ -14,7 +14,7 @@ test('Player Card fucking does something ANYTHING PLEASE', () => {
 
     const playerName = getByTestId('player-name').textContent;
     const playerCountry = getByTestId('player-country').textContent;
-    const playerSearches = getByTestId('player-searches').textContent;
+    //const playerSearches = getByTestId('player-searches').textContent;
 
     expect(fakePlayer.name).toEqual(playerName);
     expect(fakePlayer.country).toEqual(playerCountry);
